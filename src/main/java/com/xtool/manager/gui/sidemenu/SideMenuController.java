9016a4f546ff8f03bc77186b9683da8525922214
xutao <1506085843@qq.com>
2023-04-13 14:12:00 +0800

    屏蔽多余菜单

--- src/main/java/com/xtool/manager/gui/sidemenu/SideMenuController.java
+++ src/main/java/com/xtool/manager/gui/sidemenu/SideMenuController.java
@@ -23,69 +23,69 @@ public class SideMenuController {
 
     @FXMLViewFlowContext
     private ViewFlowContext context;
-    @FXML
-    @ActionTrigger("buttons")
-    private Label button;
-    @FXML
-    @ActionTrigger("checkbox")
-    private Label checkbox;
-    @FXML
-    @ActionTrigger("combobox")
-    private Label combobox;
-    @FXML
-    @ActionTrigger("dialogs")
-    private Label dialogs;
-    @FXML
-    @ActionTrigger("icons")
-    private Label icons;
-    @FXML
-    @ActionTrigger("listview")
-    private Label listview;
-    @FXML
-    @ActionTrigger("treetableview")
-    private Label treetableview;
-    @FXML
-    @ActionTrigger("progressbar")
-    private Label progressbar;
-    @FXML
-    @ActionTrigger("radiobutton")
-    private Label radiobutton;
-    @FXML
-    @ActionTrigger("slider")
-    private Label slider;
-    @FXML
-    @ActionTrigger("spinner")
-    private Label spinner;
-    @FXML
-    @ActionTrigger("textfield")
-    private Label textfield;
-    @FXML
-    @ActionTrigger("togglebutton")
-    private Label togglebutton;
-    @FXML
-    @ActionTrigger("popup")
-    private Label popup;
-    @FXML
-    @ActionTrigger("svgLoader")
-    private Label svgLoader;
-    @FXML
-    @ActionTrigger("pickers")
-    private Label pickers;
-    @FXML
-    @ActionTrigger("masonry")
-    private Label masonry;
-    @FXML
-    @ActionTrigger("scrollpane")
-    private Label scrollpane;
-    @FXML
-    @ActionTrigger("chipview")
-    private Label chipview;
-    @FXML
-    @ActionTrigger("nodeslist")
-    private Label nodesList;
-    @FXML
-    @ActionTrigger("highlighter")
-    private Label highlighter;
+//    @FXML
+//    @ActionTrigger("buttons")
+//    private Label button;
+//    @FXML
+//    @ActionTrigger("checkbox")
+//    private Label checkbox;
+//    @FXML
+//    @ActionTrigger("combobox")
+//    private Label combobox;
+//    @FXML
+//    @ActionTrigger("dialogs")
+//    private Label dialogs;
+//    @FXML
+//    @ActionTrigger("icons")
+//    private Label icons;
+//    @FXML
+//    @ActionTrigger("listview")
+//    private Label listview;
+//    @FXML
+//    @ActionTrigger("treetableview")
+//    private Label treetableview;
+//    @FXML
+//    @ActionTrigger("progressbar")
+//    private Label progressbar;
+//    @FXML
+//    @ActionTrigger("radiobutton")
+//    private Label radiobutton;
+//    @FXML
+//    @ActionTrigger("slider")
+//    private Label slider;
+//    @FXML
+//    @ActionTrigger("spinner")
+//    private Label spinner;
+//    @FXML
+//    @ActionTrigger("textfield")
+//    private Label textfield;
+//    @FXML
+//    @ActionTrigger("togglebutton")
+//    private Label togglebutton;
+//    @FXML
+//    @ActionTrigger("popup")
+//    private Label popup;
+//    @FXML
+//    @ActionTrigger("svgLoader")
+//    private Label svgLoader;
+//    @FXML
+//    @ActionTrigger("pickers")
+//    private Label pickers;
+//    @FXML
+//    @ActionTrigger("masonry")
+//    private Label masonry;
+//    @FXML
+//    @ActionTrigger("scrollpane")
+//    private Label scrollpane;
+//    @FXML
+//    @ActionTrigger("chipview")
+//    private Label chipview;
+//    @FXML
+//    @ActionTrigger("nodeslist")
+//    private Label nodesList;
+//    @FXML
+//    @ActionTrigger("highlighter")
+//    private Label highlighter;
     @FXML
     @ActionTrigger("urlEncode")
     private Label urlEncode;
@@ -101,9 +101,9 @@ public class SideMenuController {
     @FXML
     @ActionTrigger("searchWord")
     private Label searchWord;
-    @FXML
-    @ActionTrigger("lotteryTicket")
-    private Label lotteryTicket;
+//    @FXML
+//    @ActionTrigger("lotteryTicket")
+//    private Label lotteryTicket;
     @FXML
     @ActionTrigger("timeStamp")
     private Label timeStamp;
@@ -134,33 +134,33 @@ public class SideMenuController {
             }).start();
         });
         Flow contentFlow = (Flow) context.getRegisteredObject("ContentFlow");
-        bindNodeToController(button, ButtonController.class, contentFlow );
-        bindNodeToController(checkbox, CheckboxController.class, contentFlow );
-        bindNodeToController(combobox, ComboBoxController.class, contentFlow );
-        bindNodeToController(dialogs, DialogController.class, contentFlow );
-        bindNodeToController(icons, IconsController.class, contentFlow );
-        bindNodeToController(listview, ListViewController.class, contentFlow );
-        bindNodeToController(treetableview, TreeTableViewController.class, contentFlow );
-        bindNodeToController(progressbar, ProgressBarController.class, contentFlow );
-        bindNodeToController(radiobutton, RadioButtonController.class, contentFlow );
-        bindNodeToController(slider, SliderController.class, contentFlow );
-        bindNodeToController(spinner, SpinnerController.class, contentFlow );
-        bindNodeToController(textfield, TextFieldController.class, contentFlow );
-        bindNodeToController(highlighter, HighlighterController.class, contentFlow );
-        bindNodeToController(chipview, ChipViewController.class, contentFlow );
-        bindNodeToController(togglebutton, ToggleButtonController.class, contentFlow );
-        bindNodeToController(popup, PopupController.class, contentFlow );
-        bindNodeToController(svgLoader, SVGLoaderController.class, contentFlow );
-        bindNodeToController(pickers, PickersController.class, contentFlow );
-        bindNodeToController(masonry, MasonryPaneController.class, contentFlow );
-        bindNodeToController(scrollpane, ScrollPaneController.class, contentFlow );
-        bindNodeToController(nodesList, NodesListController.class, contentFlow );
+//        bindNodeToController(button, ButtonController.class, contentFlow );
+//        bindNodeToController(checkbox, CheckboxController.class, contentFlow );
+//        bindNodeToController(combobox, ComboBoxController.class, contentFlow );
+//        bindNodeToController(dialogs, DialogController.class, contentFlow );
+//        bindNodeToController(icons, IconsController.class, contentFlow );
+//        bindNodeToController(listview, ListViewController.class, contentFlow );
+//        bindNodeToController(treetableview, TreeTableViewController.class, contentFlow );
+//        bindNodeToController(progressbar, ProgressBarController.class, contentFlow );
+//        bindNodeToController(radiobutton, RadioButtonController.class, contentFlow );
+//        bindNodeToController(slider, SliderController.class, contentFlow );
+//        bindNodeToController(spinner, SpinnerController.class, contentFlow );
+//        bindNodeToController(textfield, TextFieldController.class, contentFlow );
+//        bindNodeToController(highlighter, HighlighterController.class, contentFlow );
+//        bindNodeToController(chipview, ChipViewController.class, contentFlow );
+//        bindNodeToController(togglebutton, ToggleButtonController.class, contentFlow );
+//        bindNodeToController(popup, PopupController.class, contentFlow );
+//        bindNodeToController(svgLoader, SVGLoaderController.class, contentFlow );
+//        bindNodeToController(pickers, PickersController.class, contentFlow );
+//        bindNodeToController(masonry, MasonryPaneController.class, contentFlow );
+//        bindNodeToController(scrollpane, ScrollPaneController.class, contentFlow );
+//        bindNodeToController(nodesList, NodesListController.class, contentFlow );
         bindNodeToController(urlEncode, UrlEncodeController.class, contentFlow );
         bindNodeToController(base64Encode, Base64EncodeController.class, contentFlow );
         bindNodeToController(aesEncode, AesEncodeController.class, contentFlow );
         bindNodeToController(jsonFormat, JsonFormateController.class, contentFlow );
         bindNodeToController(searchWord, SearchWordController.class, contentFlow );
-        bindNodeToController(lotteryTicket, LotteryTicketController.class, contentFlow );
+//        bindNodeToController(lotteryTicket, LotteryTicketController.class, contentFlow );
         bindNodeToController(timeStamp, TimeStampController.class, contentFlow );
     }
 
